63ba9de88761905812906e0c4154b67ff337932d
asmitakhadka <41553979+asmitakhadka@users.noreply.github.com>
2018-09-05 16:57:44 +1200

    Add files via upload

---
+++ Question2/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string[] Names = new string[5];
+            for (int i = 0; i < Names.Length; i++)
+            {
+                Console.WriteLine($"Enter the name {i + 1}: ");
+                Names[i] = Console.ReadLine();
+            }
+            Console.WriteLine($"The first named entered is {Names[0]}");
+            Console.WriteLine($"The last named entered is {Names[4]}");
+        }
+    }
+}
