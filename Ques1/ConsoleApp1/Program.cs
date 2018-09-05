a113ac15f4e1a4c9df7c04c2565ad16f9c15ffef
asmitakhadka <41553979+asmitakhadka@users.noreply.github.com>
2018-09-05 16:33:30 +1200

    Update Program.cs

--- Ques1/ConsoleApp1/Program.cs
+++ Ques1/ConsoleApp1/Program.cs
@@ -16,29 +16,29 @@ namespace ConsoleApp1
 
             string[] medalwinner = new string[3];
             Console.WriteLine("Enter the name of GOLD medal winner");
-            medalwinner[1] = Console.ReadLine();
+            medalwinner[0] = Console.ReadLine();
             Console.WriteLine("Enter the name of SILVER medal winner");
-            medalwinner[2] = Console.ReadLine();
+            medalwinner[1] = Console.ReadLine();
             Console.WriteLine("Enter the name of BRONZE medal winner");
-            medalwinner[3] = Console.ReadLine();
+            medalwinner[2] = Console.ReadLine();
 
             Console.WriteLine("Which medal holder would you like to see displayed");
-            Console.WriteLine("Press 1 for the gold medal winner");
-            Console.WriteLine("Press 2 for the silver medal winner");
-            Console.WriteLine("Press 3 for the bronze medal winner");
+            Console.WriteLine("Press 0 for the gold medal winner");
+            Console.WriteLine("Press 1 for the silver medal winner");
+            Console.WriteLine("Press 2 for the bronze medal winner");
             int input = int.Parse(Console.ReadLine());
 
-            if (input == 1)
+            if (input == 0)
             {
-                Console.WriteLine("$(Gold medal winner is: {medalwinner[1]})");
+                Console.WriteLine($"(Gold medal winner is: {medalwinner[0]})");
+            }
+            else if (input == 1)
+            {
+                Console.WriteLine($"(Silver medal winner is: {medalwinner[1]})");
             }
             else if (input == 2)
             {
-                Console.WriteLine("$(Silver medal winner is: {medalwinner[2]})");
-            }
-            else if (input == 3)
-            {
-                Console.WriteLine("$(Bronze medal winner is: {medalwinner[3]})");
+                Console.WriteLine($"(Bronze medal winner is: {medalwinner[2]})");
             }
         }
     }
